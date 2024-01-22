c72993d68bf4271900c7448f79b7b5755e33511b
Grant Erickson <grant@granterickson.com>
2024-01-22 06:49:09 -0800

    Fix quotes in strings. Add download button.

--- BibleTxtToCsv.Lib/Verse.cs
+++ BibleTxtToCsv.Lib/Verse.cs
@@ -23,7 +23,7 @@ namespace BibleTxtToCsv.Lib
 
         public string FormatCsv()
         {
-            return $@"""{Book}"",{Chapter},{VerseNumber},""{Text}""";
+            return $@"""{Book}"",{Chapter},{VerseNumber},""{Text.Replace("\"","\"\"")}""";
         }
     }
 }
